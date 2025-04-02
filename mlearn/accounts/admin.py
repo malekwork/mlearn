6891de672a6197d2a7f9e69a34a065ecd321a45b
malekwork <malek.person@gmail.com>
2025-04-02 05:42:00 +0330

    admin panel bug fixed and improved

--- mlearn/accounts/admin.py
+++ mlearn/accounts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('mobile', 'name', 'is_staff', 'is_superuser')
+    search_fields = ('mobile', 'name')
+    ordering = ('mobile',)
+    fieldsets = (
+        (None, {'fields': ('mobile', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('mobile', 'name', 'password'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+
--- mlearn/accounts/models.py
+++ mlearn/accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class UserManager(BaseUserManager):
@@ -13,15 +13,17 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, mobile, password):
         user = self.create_user(mobile, password)
-        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     mobile = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=100, null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
