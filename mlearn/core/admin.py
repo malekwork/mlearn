419f6613a0a1e7529b8bb9b92fe7caa451fc8be7
malekwork <malek.person@gmail.com>
2025-04-08 13:31:29 +0330

    comment added to admin

--- mlearn/core/admin.py
+++ mlearn/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from mptt.admin import DraggableMPTTAdmin
-from .models import Category, Post
+from .models import Category, Post, Comment
 
 admin.site.register(Category, DraggableMPTTAdmin)
 admin.site.register(Post)
+admin.site.register(Comment)
